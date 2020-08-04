2d9d2794f6e0e47e381090e2b870d12a92e21879
Ben Keener <keener4christ@gmail.com>
2020-08-04 00:56:07 -0400

    Completed task 0

---
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -0,0 +1,48 @@
+#include "binary_trees.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * binary_tree_node - Creates a binary tree node
+ *
+ * @parent: Pointer to the parent to create the child for
+ * @value: Value for the new node to have
+ *
+ * Return: Pointer to the new node, or NULL on failure
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new = malloc(sizeof(binary_tree_t));
+
+	if (!new)
+	{
+		return (NULL);
+	}
+
+	new->n = value;
+	new->left = NULL;
+	new->right = NULL;
+
+	if (!parent)
+	{
+		new->parent = NULL;
+		return (new);
+	}
+	else
+	{
+		new->parent = parent;
+	}
+	
+
+	if (!(parent->left))
+		parent->left = new;
+	else if (!(parent->right))
+		parent->right = new;
+	else
+	{
+		free(new);
+		return (NULL);
+	}
+
+	return (new);
+}
