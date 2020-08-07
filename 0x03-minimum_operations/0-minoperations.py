b9c19fd7c3943f467b140667e02a93dc3b7d89b0
Ben Keener <keener4christ@gmail.com>
2020-08-06 20:52:09 -0400

    Completed task 0

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def minoperations(n):
+    if (n <= 1):
+        return 0
+    
+    onnum = n
+    i = 2
+    total = 0
+    while (i <= onnum // 2):
+        if (onnum % i == 0):
+            total += i
+            onnum //= i
+        else:
+            i += 1
+
+    return total + onnum
+
+print(minoperations(9))
