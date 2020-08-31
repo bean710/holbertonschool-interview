83629e8861162d0187d65bda65e0ab62c5f3665b
Ben Keener <keener4christ@gmail.com>
2020-08-30 23:26:22 -0400

    Completed logging problem

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Uses string parsing to get information from a data log"""
+import sys
+import re
+
+codeSet = ("200", "301", "400", "401", "403", "404", "405", "500")
+
+it = 0
+fSize = 0
+errCodes = {}
+
+try:
+    for line in sys.stdin:
+        matches = re.match(
+            r"^[\d\.]+ - \[.+\] \"GET /projects/260 HTTP/1\.1\\\" (\d+) (\d+)",
+            line)
+
+        fSize += int(matches.group(2))
+        if (matches.group(1) in errCodes):
+            errCodes[matches.group(1)] += 1
+        else:
+            errCodes[matches.group(1)] = 1
+
+        it += 1
+
+        if (it == 5):
+            it = 0
+            print("File size: {}".format(fSize))
+
+            for code in codeSet:
+                if (code in errCodes):
+                    print("{}: {}".format(code, errCodes[code]))
+
+except KeyboardInterrupt:
+
+    print("File size: {}".format(fSize))
+
+    for code in codeSet:
+        if (code in errCodes):
+            print("{}: {}".format(code, errCodes[code]))
