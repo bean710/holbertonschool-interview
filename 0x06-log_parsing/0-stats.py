175069877163714757cd263efdc982abb37abe58
Ben Keener <keener4christ@gmail.com>
2020-08-31 11:32:46 -0400

    Added check for main

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -3,38 +3,39 @@
 import sys
 import re
 
-codeSet = ("200", "301", "400", "401", "403", "404", "405", "500")
+if __name__ == "__main__":
+    codeSet = ("200", "301", "400", "401", "403", "404", "405", "500")
 
-it = 0
-fSize = 0
-errCodes = {}
+    it = 0
+    fSize = 0
+    errCodes = {}
 
-try:
-    for line in sys.stdin:
-        matches = re.match(
-            "^[\d\.]+ - \[.+\] \"GET /projects/260 HTTP/1\.1\\\" (\d+) (\d+)",
-            line)
+    try:
+        for line in sys.stdin:
+            matches = re.match(
+                "^[\d\.]+ - \[.+\] \"GET /\w+/260 HTTP/1\.1\\\" (\d+) (\d+)",
+                line)
 
-        fSize += int(matches.group(2))
-        if (matches.group(1) in errCodes):
-            errCodes[matches.group(1)] += 1
-        else:
-            errCodes[matches.group(1)] = 1
+            fSize += int(matches.group(2))
+            if (matches.group(1) in errCodes):
+                errCodes[matches.group(1)] += 1
+            else:
+                errCodes[matches.group(1)] = 1
 
-        it += 1
+            it += 1
 
-        if (it == 10):
-            it = 0
-            print("File size: {}".format(fSize))
+            if (it == 10):
+                it = 0
+                print("File size: {}".format(fSize))
 
-            for code in codeSet:
-                if (code in errCodes):
-                    print("{}: {}".format(code, errCodes[code]))
+                for code in codeSet:
+                    if (code in errCodes):
+                        print("{}: {}".format(code, errCodes[code]))
 
-except KeyboardInterrupt:
+    except KeyboardInterrupt:
 
-    print("File size: {}".format(fSize))
+        print("File size: {}".format(fSize))
 
-    for code in codeSet:
-        if (code in errCodes):
-            print("{}: {}".format(code, errCodes[code]))
+        for code in codeSet:
+            if (code in errCodes):
+                print("{}: {}".format(code, errCodes[code]))
