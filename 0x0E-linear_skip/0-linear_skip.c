8d7f0af99cff0fc71b2ca8f462e2c3998e84bda8
Ben Keener <keener4christ@gmail.com>
2020-11-18 16:08:55 -0500

    Fixed lookahead skipping

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -17,7 +17,7 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 	if (!list)
 		return (NULL);
 
-	while (search && search->express != NULL && search->n <= value)
+	while (search && search->express != NULL && search->n < value)
 	{
 		on = search;
 		search = on->express;
