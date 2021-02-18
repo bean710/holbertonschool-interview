f12bf0397f20cd20ec367f1fcce97ed512c14934
Ben Keener <keener4christ@gmail.com>
2021-02-18 17:48:24 -0500

    Completed algorithm

---
+++ 0x18-merge_sort/0-merge_sort.c
@@ -0,0 +1,84 @@
+#include "sort.h"
+
+/**
+ * merge_sort - Sorts an array using merge sort
+ *
+ * @array: Pointer to the first element of the array to be sorted
+ * @size: The size of the array to sort
+**/
+void merge_sort(int *array, size_t size)
+{
+	int *temp;
+
+	if (!array || size <= 1)
+		return;
+
+	temp = malloc(sizeof(int) * size);
+
+	for (size_t i = 0; i < size; i++)
+		temp[i] = array[i];
+
+	top_down(array, temp, 0, size);
+
+	free(temp);
+}
+
+/**
+ * top_down - Splits the array from top-down, then merges recursively
+ *
+ * @temp: Pointer to the start of the array to modify
+ * @array: Pointer to the start of the array to reference
+ * @start: The index to start split at
+ * @end: The index to end split at (exclusive)
+**/
+void top_down(int *temp, int *array, size_t start, size_t end)
+{
+	size_t middle;
+
+	if (end - start <= 1)
+		return;
+
+	middle = (start + end) / 2;
+
+	top_down(array, temp, start, middle);
+	top_down(array, temp, middle, end);
+
+	merge(temp, array, start, end, middle);
+}
+
+/**
+ * merge - Merge two arrays back together
+ *
+ * @array: Pointer to the reference array
+ * @temp: Pointer to the array to modify
+ * @start: Index to start at (beginning of right leg)
+ * @end: Index to end at (exclusive) (end of left leg)
+ * @middle: The index the two legs are split at (end of right, start of left)
+**/
+void merge(int *array, int *temp, size_t start, size_t end, size_t middle)
+{
+	size_t i = start, j = middle;
+
+	printf("Merging...\n");
+	printf("[left]: ");
+	print_array(array + start, middle - start);
+	printf("[right]: ");
+	print_array(array + middle, end - middle);
+
+	for (size_t k = start; k < end; k++)
+	{
+		if (i < middle && (j >= end || array[i] <= array[j]))
+		{
+			temp[k] = array[i];
+			i++;
+		}
+		else
+		{
+			temp[k] = array[j];
+			j++;
+		}
+	}
+
+	printf("[Done]: ");
+	print_array(temp + start, end - start);
+}
