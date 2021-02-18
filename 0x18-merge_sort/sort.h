466983318e60a07344662e85dfbf9fa2137a73df
Ben Keener <keener4christ@gmail.com>
2021-02-18 18:47:04 -0500

    Fixed variables

--- 0x18-merge_sort/sort.h
+++ 0x18-merge_sort/sort.h
@@ -3,7 +3,7 @@
 
 void print_array(const int *array, size_t size);
 void merge_sort(int *array, size_t size);
-void top_down(int *array, int *temp, size_t start, size_t end);
+void top_down(int *temp, int *array, size_t start, size_t end);
 void merge(int *array, int *temp, size_t start, size_t end, size_t middle);
 
 #endif
